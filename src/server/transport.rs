23589cae720fdf26adb7b4e50bab45482a4051bc
u8array <alexanderleinich1@gmail.com>
2025-10-09 21:53:37 +0200

    Remove unused `run` function and clean up imports in transport module

--- src/server/transport.rs
+++ src/server/transport.rs
@@ -3,7 +3,6 @@ use jsonrpc_v2::{Data, MapRouter, Server};
 use reqwest::Client;
 use std::io;
 use std::sync::Arc;
-use std::time::Duration;
 use tokio::io::BufReader;
 
 use super::rpc;
@@ -24,14 +23,4 @@ pub async fn run_with_server(server: Arc<Server<jsonrpc_v2::MapRouter>>) -> Resu
     let stdin = BufReader::new(tokio::io::stdin());
     let stdout = io::stdout();
     crate::server::io::run_with_io(server, stdin, stdout).await
-}
-
-pub async fn run() -> Result<()> {
-    let client = Client::builder()
-        .timeout(Duration::from_secs(10))
-        .redirect(reqwest::redirect::Policy::limited(10))
-        .user_agent("lm_mcp_server/0.1.0")
-        .build()?;
-    let server = build_server(&client);
-    run_with_server(server).await
 }
